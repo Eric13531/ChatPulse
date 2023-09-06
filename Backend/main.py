38f20c544b03739e69a5b37e75a89098f049619a
Christian Aiello <93229991+christian-aiello@users.noreply.github.com>
2023-09-06 16:24:48 -0400

    Update requirements.txt

--- Backend/main.py
+++ Backend/main.py
@@ -2,38 +2,50 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
 
-from Database.crud import add_room, add_user_to_room, remove_user_from_room, delete_room, get_users_from_room, get_room_id_from_user
+from Database.crud import (
+    add_room,
+    add_user_to_room,
+    remove_user_from_room,
+    delete_room,
+    get_users_from_room,
+    get_room_id_from_user,
+)
 from Database.config import client
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+app.config["SECRET_KEY"] = "vnkdjnfjknfl1232#"
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
-@socketio.on('connect')
-def onConnectCallback(sid, data):
-    socketio.emit('acknowledge', data = {'status': 'success'}, room = sid)
 
-@socketio.on('connected')
+@socketio.on("connect")
+def onConnectCallback(sid, data):
+    socketio.emit("acknowledge", data={"status": "success"}, room=sid)
+
+
+@socketio.on("connected")
 def onConnectCallback(data):
     add_user_to_room(client, request.sid, data.room)
-    socketio.emit('connectCallback', {'data': request.sid }, room= request.sid)
+    socketio.emit("connectCallback", {"data": request.sid}, room=request.sid)
 
-@socketio.on('disconnect')
+
+@socketio.on("disconnect")
 def onDisconnectCallback(sid, data):
-  remove_user_from_room(client, sid, data.room)
+    remove_user_from_room(client, sid, data.room)
 
-@socketio.on('message')
+
+@socketio.on("message")
 def onMessageCallback(data):
-  sid = request.sid
-  response = get_room_id_from_user(sid)
-  if (response == 0):
-    return
-  else:
-     for id in response:
-        if (id == sid):
-          continue
-        socketio.emit('message', {'data': data.message}, room = id)
+    sid = request.sid
+    response = get_room_id_from_user(sid)
+    if response == 0:
+        return
+    else:
+        for id in response:
+            if id == sid:
+                continue
+            socketio.emit("message", {"data": data.message}, room=id)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     socketio.run(app, debug=True)
